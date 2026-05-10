b64bd4cfc46b259df0eb881fdeec56b6b8fd2c5f
Hrushikesh Makode <43725762+HrushiMakode@users.noreply.github.com>
2026-05-10 15:27:10 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+   public:
+
+    int dp(int n,vector<int> &memo){
+        if(n<=2) return n;
+        if(memo[n]!=-1) return memo[n];
+        return memo[n] = dp(n-1,memo) + dp(n-2,memo);
+    }
+
+    int climbStairs(int n) {
+        vector<int> memo(n+1,-1);
+        return dp(n,memo);
+    }
+};
